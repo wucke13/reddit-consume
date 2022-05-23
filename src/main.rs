33605625ccd6cdf51dd15f875c1c4fa74a1680c6
wucke13 <wucke13@gmail.com>
2022-05-23 22:57:09 +0200

    add refilling semantic

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ use std::process::{Command, Stdio};
 
 use clap::Parser;
 use mpvipc::*;
+
 use roux::util::{FeedOption, TimePeriod};
 
 //use reddit::{RedditRequest, SortBy, Timeslot};
 
 #[derive(Debug, clap::Parser)]
 #[clap(about, author, version)]
-struct Opts {
+struct Args {
     #[clap()]
     subreddit: String,
 
     #[clap(subcommand)]
     sort_by: SortBy,
+
+    /// Minimum number of submissions in the playlist before fetching new elements
+    #[clap(short, long, default_value = "20")]
+    min_buffer_size: usize,
+
+    /// Number of elements to fetch when buffer-size is reached
+    #[clap(short = 'i', long, default_value = "20")]
+    buffer_increase: usize,
 }
 
 #[derive(Debug, clap::Subcommand, Clone, Copy)]
@@ -50,10 +59,46 @@ impl Into<TimePeriod> for Period {
     }
 }
 
+async fn request(
+    subreddit: &str,
+    sort_by: SortBy,
+    limit: u32,
+    after: Option<String>,
+) -> anyhow::Result<(Vec<String>, Option<String>)> {
+    let sub = roux::subreddit::Subreddit::new(subreddit);
+
+    let mut options = FeedOption::new().limit(limit);
+    options.after = after;
+
+    if let SortBy::Top(p) = sort_by {
+        options.period = Some(p.into());
+    }
+
+    let options = Some(options);
+    let result = match sort_by {
+        SortBy::Hot => sub.hot(limit, options).await,
+        SortBy::Top(_) => sub.top(limit, options).await,
+        SortBy::New => sub.latest(limit, options).await,
+        SortBy::Rising => sub.rising(limit, options).await,
+    }?;
+    let after = result.data.after.clone();
+
+    Ok((
+        result
+            .data
+            .children
+            .iter()
+            .map(|c| c.data.url.as_ref())
+            .filter_map(std::convert::identity)
+            .cloned()
+            .collect(),
+        after,
+    ))
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // io::Result<()> {
-    let opts = Opts::parse();
+    let opts = Args::parse();
 
     let ipc_socket = "/tmp/mpvsocket";
 
@@ -67,31 +112,20 @@ async fn main() -> anyhow::Result<()> {
         .spawn()
         .expect("failed to execute child");
 
-    let sub = roux::subreddit::Subreddit::new(&opts.subreddit);
-    let limit = 50;
-    let options = match opts.sort_by {
-        SortBy::Top(p) => Some(FeedOption::new().period(p.into())),
-        _ => None,
-    };
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    let mut mpv = Mpv::connect("/tmp/mpvsocket").unwrap();
 
-    let submissions = match opts.sort_by {
-        SortBy::Hot => sub.hot(limit, options).await,
-        SortBy::Top(_) => sub.top(limit, options).await,
-        SortBy::New => sub.latest(limit, options).await,
-        SortBy::Rising => sub.rising(limit, options).await,
-    }?;
+    let mut after = None;
+    let (new, new_after) = request(
+        &opts.subreddit,
+        opts.sort_by,
+        opts.min_buffer_size as u32,
+        after.clone(),
+    )
+    .await?;
+    after = new_after;
 
-    //
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    let mpv = Mpv::connect("/tmp/mpvsocket")?;
-
-    for url in submissions
-        .data
-        .children
-        .iter()
-        .map(|c| c.data.url.as_ref())
-        .filter_map(std::convert::identity)
-    {
+    for url in new {
         println!("adding {url}");
         mpv.playlist_add(
             &url,
@@ -101,6 +135,33 @@ async fn main() -> anyhow::Result<()> {
     }
 
     mpv.playlist_play_id(0)?;
+    loop {
+        match mpv.event_listen()? {
+            Event::EndFile => {
+                let p_len: usize = mpv.get_property("playlist-count")?;
+                let p_pos: usize = mpv.get_property("playlist-pos")?;
+                if p_len - p_pos <= opts.min_buffer_size {
+                    println!("after = {after:?}");
+                    let (new, new_after) = request(
+                        &opts.subreddit,
+                        opts.sort_by,
+                        opts.buffer_increase as u32,
+                        after.clone(),
+                    )
+                    .await?;
+                    after = new_after;
 
-    Ok(())
+                    for url in new {
+                        println!("adding {url}");
+                        mpv.playlist_add(
+                            &url,
+                            PlaylistAddTypeOptions::File,
+                            PlaylistAddOptions::Append,
+                        )?;
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
 }
