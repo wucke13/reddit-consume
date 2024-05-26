8651aa6811c651f81680beaea1553adee574a01e
wucke13 <wucke13@gmail.com>
2024-05-26 02:26:58 +0200

    fix: remove i686, compilation doesn't work

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }@inputs:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
